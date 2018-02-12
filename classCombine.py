c6617032264425cfc418e72c53b040ca35af0612
glemoine62 <guido.lemoine@ec.europa.eu>
2018-02-12 22:51:39 +0100

    Updated generic classCombine

--- classCombine.py
+++ classCombine.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import numpy as np
+import sys
+
+from collections import Counter
 
 # The join of the first and second run will include ALL parcels,
 # because the outer join of df0 and df1 introduce the 20% subset of
 # training parcels that were used in run 0 and not in run 1
-df0 = pd.read_csv('DK2017_cropselect_0_class.csv', index_col = 0)
-df1 = pd.read_csv('DK2017_cropselect_1_class.csv', index_col = 0)
+root = sys.argv[1]
+
+nchunks = int(sys.argv[2])
+
+df0 = pd.read_csv('{}_cropselect_0_class.csv'.format(root), index_col = 0, low_memory=False)
+df1 = pd.read_csv('{}_cropselect_1_class.csv'.format(root), index_col = 0, low_memory=False)
 
 # Create the join and retain 'klass' label as 'klass_1'
 df = df0.join(df1, how="outer", rsuffix= '_1')
@@ -14,13 +22,26 @@ df['klass'].loc[df['klass'].isnull()] = df['klass_1'].loc[df['klass'].isnull()]
 # and drop the now redundant 'klass_1' label
 df.drop('klass_1', axis=1, inplace=True)
 
-# Load the other runs
-df2 = pd.read_csv('DK2017_cropselect_2_class.csv', index_col = 0)
-df2.drop('klass', axis=1, inplace=True)
-df3 = pd.read_csv('DK2017_cropselect_3_class.csv', index_col = 0)
-df3.drop('klass', axis=1, inplace=True)
-df4 = pd.read_csv('DK2017_cropselect_4_class.csv', index_col = 0)
-df4.drop('klass', axis=1, inplace=True)
+for i in range(2,nchunks):
+  # Load the other runs
+  print i
+  dfN = pd.read_csv('{}_cropselect_{}_class.csv'.format(root, i), index_col = 0, low_memory=False)
+  dfN.drop('klass', axis=1, inplace=True)
+  df = df.join(dfN, how="outer")  
 
 # Join the latter, overwrite NA values and save as int
-df.join(df2, how="outer").join(df3, how="outer").join(df4, how="outer").fillna(-1).astype(int).to_csv('DK2017_cropselect_classes.csv')
+
+df.fillna(-1, inplace=True)
+
+r_index = df.columns[1:]
+
+# def majority(args):
+# 	(values, counts) = np.unique(np.array(args[r_index]), return_counts =True)
+# 	ind =np.argmax(counts)
+# 	return pd.Series({'majclass': ind, 'majcount': np.max(counts)})
+
+
+df['majclass'] = df.apply(lambda x: Counter(x[r_index]).most_common(1)[0][0], axis=1)
+df['majcount'] = df.apply(lambda x: Counter(x[r_index]).most_common(1)[0][1], axis=1)
+
+df.astype(int).to_csv('{}_cropselect_classes.csv'.format(root))
